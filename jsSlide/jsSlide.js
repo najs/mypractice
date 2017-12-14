022f0fff272ac1222b727db35d12770097c1dda0
joa.na <joa.na@cheilpengtai.com>
2017-12-14 18:58:02 +0900

    commit

---
+++ jsSlide/jsSlide.js
@@ -0,0 +1,37 @@
+//(function(){
+	var jsSlide = (function(container,setting){
+		//"use strict";
+		var defaults = {
+			slideWrap : setting.slideWrapClass,
+			slideItem : setting.slideItemClass || '.jsSlide-item',
+
+		};
+
+
+		var init = {
+
+
+
+		};
+
+
+
+
+		function jsSlide(container, options){
+			var Container = $(container);
+
+
+		}
+
+	});
+
+
+
+var aa = new jsSlide("#slide0",{
+	effectMode :'sliding',
+	firstIdx : 0,
+	loop : false
+});
+//}());
+
+
