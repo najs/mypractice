a0109ab92e72899a2466012adc9779914b32ae58
joa.na <joa.na@cheilpengtai.com>
2017-12-29 14:11:23 +0900

    commit

--- jsSlide/jsSlide.js
+++ jsSlide/jsSlide.js
@@ -1,36 +1,65 @@
 //(function(){
-	var jsSlide = (function(container,setting){
-		//"use strict";
-		var defaults = {
-			slideWrap : setting.slideWrapClass,
-			slideItem : setting.slideItemClass || '.jsSlide-item',
+var jsSlide = (function(container,options){
+	"use strict";
 
-		};
+	var defaults = {
+		slideContainter : $(container),
+		slideWrap : options.slideWrapClass,
+		slideItem : options.slideItemClass || '.jsSlide-item',
+		pageClass : options.pageClass || '.jsSlide-paging',
+		btnNextClass : options.btnNext || '.jsSlide-btn-next',
+		btnPrevClass : options.btnPrev || '.jsSlide-btn-prev',
+		btnStopClass : options.btnStop || '.jsSlide-btn-stop',
+		pastIdx : 0,
+		presentIdx : options.firstIdx || 0
+	};
+
+	var set = {
+		slideItemsLength :	defaults.slideContainter.find(defaults.slideItem).length,
+		idx : 0,
+	};
 
 
-		var init = {
-
-
-
-		};
-
-
-
-
-		function jsSlide(container, options){
-			var Container = $(container);
-
-
+	var init = {
+		run : function(){
+			console.log(set.slideItemsLength);
 		}
+	};
 
-	});
+	init.run();
 
 
 
-var aa = new jsSlide("#slide0",{
-	effectMode :'sliding',
-	firstIdx : 0,
-	loop : false
+	function jsSlide(container, options){
+		var Container = $(container);
+
+
+
+
+	}
+
+
+	function next(){
+		defaults.pastIdx = defaults.presentIdx;
+		defaults.presentIdx = defaults.presentIdx <= 0 ? 0 : this.defaults.presentIdx+1;
+	}
+
+	function prev(){
+		defaults.pastIdx = defaults.presentIdx;
+		defaults.presentIdx = defaults.presentIdx <= 0 ? 0 : this.defaults.presentIdx-1;
+	}
+
+
+
+
+});
+
+
+
+
+
+var slide0 = new jsSlide("#slide0",{
+	/* firstIdx : 0 */
 });
 //}());
 
