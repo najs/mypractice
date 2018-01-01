84b0df265647a7a5a5f3ddeba71b9ab1715b53fb
najs <na.jss@daum.net>
2018-01-01 17:41:14 +0900

    add moving

--- jsSlide/jsSlide.js
+++ jsSlide/jsSlide.js
@@ -1,28 +1,48 @@
-//(function(){
+
 var jsSlide = (function(container,options){
 	"use strict";
 
 	var defaults = {
 		slideContainter : $(container),
-		slideWrap : options.slideWrapClass,
+		slideList : options.slideWrapClass || '.jsSlide-list',
 		slideItem : options.slideItemClass || '.jsSlide-item',
-		pageClass : options.pageClass || '.jsSlide-paging',
-		btnNextClass : options.btnNext || '.jsSlide-btn-next',
-		btnPrevClass : options.btnPrev || '.jsSlide-btn-prev',
-		btnStopClass : options.btnStop || '.jsSlide-btn-stop',
+		paging : options.pageClass || '.jsSlide-paging',
+		btnNext : options.btnNext || '.jsSlide-btn-next',
+		btnPrev : options.btnPrev || '.jsSlide-btn-prev',
+		btnStop : options.btnStop || '.jsSlide-btn-stop',
 		pastIdx : 0,
 		presentIdx : options.firstIdx || 0
 	};
 
-	var set = {
-		slideItemsLength :	defaults.slideContainter.find(defaults.slideItem).length,
-		idx : 0,
+	var setting = {
+		slideItemsLength :	defaults.slideContainter.find(defaults.slideItem).length
 	};
 
 
+	var clickEv = {
+		next : function(){
+			defaults.slideContainter.find(defaults.btnNext).on('click',function(e){
+				e.preventDefault();
+				next();
+				moving();
+				console.log('다음!!!',defaults.presentIdx)
+			})
+		},
+		prev : function(){
+			defaults.slideContainter.find(defaults.btnPrev).on('click',function(e){
+				e.preventDefault();
+				prev();
+				moving();
+				console.log('이전!!!',defaults.presentIdx)
+			})
+		}
+	};
+
 	var init = {
 		run : function(){
-			console.log(set.slideItemsLength);
+			clickEv.next();
+			clickEv.prev();
+
 		}
 	};
 
@@ -30,27 +50,21 @@ var jsSlide = (function(container,options){
 
 
 
-	function jsSlide(container, options){
-		var Container = $(container);
-
-
-
-
-	}
-
-
 	function next(){
 		defaults.pastIdx = defaults.presentIdx;
-		defaults.presentIdx = defaults.presentIdx <= 0 ? 0 : this.defaults.presentIdx+1;
+		defaults.presentIdx = defaults.presentIdx >= setting.slideItemsLength-1 ? defaults.presentIdx : defaults.presentIdx+1;
 	}
 
 	function prev(){
 		defaults.pastIdx = defaults.presentIdx;
-		defaults.presentIdx = defaults.presentIdx <= 0 ? 0 : this.defaults.presentIdx-1;
+		defaults.presentIdx = defaults.presentIdx <= 0 ? 0 : defaults.presentIdx-1;
 	}
 
-
-
+	function moving(){
+		defaults.slideContainter.find(defaults.slideList).css({
+			transform: 'translate('+-600*defaults.presentIdx+"px,0)"
+		})
+	}
 
 });
 
@@ -60,7 +74,10 @@ var jsSlide = (function(container,options){
 
 var slide0 = new jsSlide("#slide0",{
 	/* firstIdx : 0 */
+	btnNext : '.jsSlide-btn.next',
+	btnPrev : '.jsSlide-btn.prev'
+
 });
-//}());
+
 
 
