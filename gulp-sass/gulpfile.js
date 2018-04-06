36d4f36d96e765f1366fdeaa74a12e466a2d17e4
joa.na <joa.na@cheilpengtai.com>
2018-04-06 17:58:13 +0900

    modified

--- gulp-sass/gulpfile.js
+++ gulp-sass/gulpfile.js
@@ -5,33 +5,48 @@ var sass = require('gulp-sass');
 var postcss = require('gulp-postcss');
 var sourcemaps = require('gulp-sourcemaps');
 var autoprefixer  = require('autoprefixer');
+var watch = require('gulp-watch');
 
-gulp.task('sass',function(){
-	var scssOptions = {
-		outputStyle : "compact",  // compressed, expanded, compact, nested
-		indentType : "tab"
-	};
 
-	var processors = [
-		autoprefixer({
-			browsers: [
+var isProd = true;
+
+var scssOptions = {
+	outputStyle : isProd ? "compact": 'compressed',  // compressed, expanded, compact, nested
+	indentType : "tab"
+};
+
+var processors = [
+	autoprefixer({
+		browsers: [
 			'last 2 versions', "Edge > 0", "ie >= 8", "safari >=5", "iOS >=5",
 			"Firefox >=3.0","Opera >=10.0", "Android > 0","iOS > 0","FirefoxAndroid > 0"],
-			cascade: false
-		})
-	];
+		cascade: false
+	})
+];
 
-	var paths = ['src/scss/global/reset.scss','src/scss/sample.scss'];
+gulp.task('base-sass',function(){
+	var base_paths = ['src/utils/scss_globalvars/base.scss'];
+	return gulp.src(base_paths).
+	pipe(sass(scssOptions).on('error', sass.logError)).
+	pipe(sourcemaps.init()).
+	pipe(postcss(processors)).
+	pipe(gulp.dest('src/common/css'));
+});
+
+
+
+gulp.task('sass',function(){
+	var paths = ['src/components/dev/scss/component.scss'];
 
 	return gulp.src(paths).
 	pipe(sass(scssOptions).on('error', sass.logError)).
 	pipe(sourcemaps.init()).
 	pipe(postcss(processors)).
-	pipe(gulp.dest('src/css'));
+	pipe(gulp.dest('src/components/css/'));
 });
 
 gulp.task('sass:watch', function () {
-	gulp.watch('src/scss', ['sass']);
+	gulp.watch('src/components/dev/scss', ['sass']);
 });
 
 gulp.task('compile', ['sass'], function(){});
